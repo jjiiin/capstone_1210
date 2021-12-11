562f0bd9df6ab4186923699028857db1cdc5719e
whk06061 <63198157+whk06061@users.noreply.github.com>
2021-12-11 15:57:40 +0900

    회원가입시 회원정보 firebase에 저장

--- app/src/main/java/com/capstone_design/a1209_app/utils/FBRef.kt
+++ app/src/main/java/com/capstone_design/a1209_app/utils/FBRef.kt
@@ -1,5 +1,6 @@
 package com.capstone_design.a1209_app.utils
 
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
@@ -9,7 +10,7 @@ class FBRef {
         private val database = Firebase.database
 
         val boardRef=database.getReference("contents")
-
+        val joinRef = database.getReference("users")
     }
 
 
