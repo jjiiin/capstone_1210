9a2e3d53eab37bd2fb06060ebf659118a5e340fa
jjiiin <wjdwlin4051@gmail.com>
2022-01-02 23:53:18 +0900

    커밋

--- app/src/main/java/com/capstone_design/a1209_app/board/LvAdpater.kt
+++ app/src/main/java/com/capstone_design/a1209_app/board/LvAdpater.kt
@@ -36,8 +36,7 @@ class LvAdpater(private val boardList:MutableList<dataModel>):BaseAdapter() {
 //        val cv_fee: TextView=converView!!.findViewById(R.id.item_fee)
 //        val cv_time: TextView=converView!!.findViewById(R.id.item_time)
 //        val cv_person: TextView=converView!!.findViewById(R.id.item_person)
-
-
+        // 왜 커밋이 안되지
         val cv_title=converView!!.findViewById<TextView>(R.id.item_title)
         val cv_img=converView!!.findViewById<ImageView>(R.id.item_image)
         val cv_place=converView!!.findViewById<TextView>(R.id.item_place)
