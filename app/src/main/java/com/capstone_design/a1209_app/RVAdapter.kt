815176644ab588299568d9485bff857d634c8383
jjiiin <wjdwlin4051@gmail.com>
2022-02-22 21:03:33 +0900

    recyclerview update

---
+++ app/src/main/java/com/capstone_design/a1209_app/RVAdapter.kt
@@ -0,0 +1,48 @@
+package com.capstone_design.a1209_app
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.capstone_design.a1209_app.dataModels.addressData
+
+class RVAdapter (val items:MutableList<addressData>): RecyclerView.Adapter<RVAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view= LayoutInflater.from(parent.context).inflate(R.layout.address_item,parent,false)
+        return ViewHolder(view)
+    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item=items[position]
+        holder.name.text=item.name
+        holder.detail.text=item.address+" "+item.detail
+        if(item.set){
+            holder.itemView.setBackgroundColor(Color.parseColor("#c4c4c4"))// 완료한 건 색상 변경하기
+        }
+
+        holder.itemView.setOnClickListener {
+            itemClickListener.onClick(it, position)
+        }
+    }
+    // (2) 리스너 인터페이스
+    interface OnItemClickListener{
+        fun onClick(v: View, position: Int)
+    }
+    // (3) 외부에서 클릭 시 이벤트 설정
+    fun setItemClickListener(onItemClickListener: OnItemClickListener)  {
+        this.itemClickListener = onItemClickListener
+    }
+    // (4) setItemClickListener로 설정한 함수 실행
+    private lateinit var itemClickListener : OnItemClickListener
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+    inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        val name: TextView =itemView.findViewById(R.id.name)
+        val detail: TextView =itemView.findViewById(R.id.detail)
+    }
+}
---
+++ app/src/main/java/com/capstone_design/a1209_app/dataModels/addressData.kt
@@ -0,0 +1,8 @@
+package com.capstone_design.a1209_app.dataModels
+
+data class addressData(
+    val address:String="",
+    val detail:String="",
+    val name:String="",
+    val set:Boolean=false
+)
