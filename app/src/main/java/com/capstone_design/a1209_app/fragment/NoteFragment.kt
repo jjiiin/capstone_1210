1292266b30b019e759388eb0b1b60126fae647ea
whk06061 <63198157+whk06061@users.noreply.github.com>
2022-05-06 18:03:05 +0900

    NoteFragment 휴지통 반영

--- app/src/main/java/com/capstone_design/a1209_app/fragment/NoteFragment.kt
+++ app/src/main/java/com/capstone_design/a1209_app/fragment/NoteFragment.kt
@@ -33,6 +33,7 @@ class NoteFragment : Fragment() {
     //몇 번째 탭이 선택됐는지
     var selectedTab: Int = 0
     var isClick = 0
+    var isClick2 = 0
 
     companion object {
         //휴지통 버튼 눌린것에따라 내부 프래그먼트에서 처리해주기위해
